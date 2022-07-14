5e3ac7d692b6a2c101a66a449200b3be7c9004a1
Mohamed jinas <j@live.mv>
2022-07-14 08:29:05 +0500

    toggle component

---
+++ components/form/toggle.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+    },
+    value: Boolean,
+    label: String,
+    error: String,
+    required: {
+      default: 'required',
+    },
+  },
+}
+</script>
+<template>
+  <div class="mb-4">
+    <label class="block text-sm font-medium text-gray-700" v-if="label">
+      <span>{{ label }}</span>
+    </label>
+    <span @click="$emit('input', !value)" role="checkbox" tabindex="0" aria-checked="false" class="relative inline-flex items-center justify-center flex-shrink-0 w-10 h-5 cursor-pointer group focus:outline-none">
+      <!-- On: "bg-indigo-600", Off: "bg-gray-200" -->
+      <span aria-hidden="true" :class="{'bg-green-400': value}" class="absolute h-4 mx-auto bg-gray-200 rounded-full w-9 transition-colors ease-in-out duration-200"></span>
+      <!-- On: "translate-x-5", Off: "translate-x-0" -->
+      <span aria-hidden="true" :class="{'translate-x-5': value}" class="absolute left-0 inline-block w-5 h-5 bg-white border border-gray-200 rounded-full shadow translate-x-0 transform group-focus:shadow-outline group-focus:border-blue-300 transition-transform ease-in-out duration-200"></span>
+    </span>
+    <div v-if="error">
+      <p class="mt-2 text-sm text-red-600">
+      {{ error }}
+      </p>
+    </div>
+  </div>
+</template>
