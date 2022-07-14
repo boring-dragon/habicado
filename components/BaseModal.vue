68eb09a2b28587393e4acebe6d82655e872123b7
Mohamed jinas <j@live.mv>
2022-07-14 07:23:40 +0500

    Modal and Textarea Component

---
+++ components/BaseModal.vue
@@ -0,0 +1,95 @@
+<template>
+	<teleport to="body">
+		<transition leave-active-class="duration-200">
+			<div class="fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50" scroll-region v-show="show">
+				<transition enter-active-class="ease-out duration-300" enter-from-class="opacity-0" enter-to-class="opacity-100" leave-active-class="ease-in duration-200" leave-from-class="opacity-100" leave-to-class="opacity-0">
+					<div @click="close" class="fixed inset-0 transform transition-all" v-show="show">
+						<div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+					</div>
+				</transition>
+
+				<transition enter-active-class="ease-out duration-300" enter-from-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to-class="opacity-100 translate-y-0 sm:scale-100" leave-active-class="ease-in duration-200" leave-from-class="opacity-100 translate-y-0 sm:scale-100" leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+					<div :class="maxWidthClass" class="mb-6 bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full sm:mx-auto" v-show="show">
+						<slot v-if="show"></slot>
+					</div>
+				</transition>
+			</div>
+		</transition>
+	</teleport>
+</template>
+
+<script>
+export default {
+  emits: ["close"],
+
+  props: {
+    show: {
+      default: false,
+    },
+    maxWidth: {
+      default: "2xl",
+    },
+    closeable: {
+      default: true,
+    },
+  },
+
+  watch: {
+    show: {
+      immediate: true,
+      handler: (show) => {
+        if (show) {
+          document.body.style.overflow = "hidden";
+        } else {
+          document.body.style.overflow = null;
+        }
+      },
+    },
+  },
+
+  mounted() {
+    const close = () => {
+      if (this.closeable) {
+        emit("close");
+      }
+    };
+    const closeOnEscape = (e) => {
+      if (e.key === "Escape" && this.show) {
+        close();
+      }
+    };
+
+    document.addEventListener("keydown", closeOnEscape);
+  },
+
+  destroyed() {
+    document.removeEventListener("keydown", closeOnEscape);
+    document.body.style.overflow = null;
+  },
+
+  methods: {
+    close() {
+      if (this.closeable) {
+        emit("close");
+      }
+    },
+  },
+
+  computed: {
+    maxWidthClass() {
+      return {
+        sm: "sm:max-w-sm",
+        md: "sm:max-w-md",
+        lg: "sm:max-w-lg",
+        xl: "sm:max-w-xl",
+        "2xl": "sm:max-w-2xl",
+        "3xl": "sm:max-w-3xl",
+        "4xl": "sm:max-w-4xl",
+        "5xl": "sm:max-w-5xl",
+        "6xl": "sm:max-w-6xl",
+        "7xl": "sm:max-w-7xl",
+      }[this.maxWidth];
+    },
+  },
+};
+</script>
---
+++ components/form/textarea.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: "FormTextarea",
+  props: {
+    rows: {
+      type: Number,
+      default: 4,
+    },
+  },
+  methods: {
+    handleInput(e) {
+      this.$emit("input", e.target.value);
+    },
+  },
+};
+</script>
+<template>
+    <textarea @input="handleInput" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" :rows="rows"></textarea>
+</template>
