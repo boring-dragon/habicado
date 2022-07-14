5460702b6da4568dee93da3bfdbc9cc5bfb331b7
Mohamed jinas <j@live.mv>
2022-07-14 08:56:43 +0500

    WIP

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,65 +1,56 @@
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+	// Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+	ssr: false,
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'habit-tracker',
-    htmlAttrs: {
-      lang: 'en'
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	head: {
+		title: "habit-tracker",
+		htmlAttrs: {
+			lang: "en",
+		},
+		meta: [
+			{ charset: "utf-8" },
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1",
+			},
+			{ hid: "description", name: "description", content: "" },
+			{ name: "format-detection", content: "telephone=no" },
+		],
+		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+	},
 
-  env: {
-    backendUrl: process.env.NUXT_PUBLIC_BACKEND_URL
-  },
+	env: {
+		backendUrl: process.env.NUXT_PUBLIC_BACKEND_URL,
+	},
 
-  googleFonts: {
-    families: {
-      Poppins: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-    }
-  },
+	googleFonts: {
+		families: {
+			Poppins: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+		},
+	},
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '@/assets/css/main.css',
-  ],
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: ["@/assets/css/main.css"],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    '@nuxt/postcss8',
-    '@nuxtjs/google-fonts'
-  ],
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: ["@nuxt/postcss8", "@nuxtjs/google-fonts"],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/auth-next'
-  ],
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: ["@nuxtjs/axios", "@nuxtjs/auth-next"],
 
-  axios: {
-    baseURL: process.env.NUXT_PUBLIC_BACKEND_URL,
-    credentials: true
-  },
+	axios: {
+		baseURL: process.env.NUXT_PUBLIC_BACKEND_URL,
+		credentials: true,
+	},
 
-
-  /*
+	/*
   auth: {
     strategies: {
       laravelSanctum: {
@@ -75,13 +66,13 @@ export default {
 
   */
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    postcss: {
-      plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-      },
-    },
-  }
-}
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {
+		postcss: {
+			plugins: {
+				tailwindcss: {},
+				autoprefixer: {},
+			},
+		},
+	},
+};
