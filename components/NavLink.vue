8fcbc3ef1c555b949e09f4bd09dd7e98c122fb9b
Mohamed jinas <j@live.mv>
2022-07-28 11:16:03 +0500

    WIP

--- components/NavLink.vue
+++ components/NavLink.vue
@@ -18,7 +18,7 @@ export default {
 </script>
 
 <template>
-	<NuxtLink :class="active ? 'text-white bg-primary': 'text-primary'" :to="to" class="group relative min-w-0 flex-1 bg-secondary py-4 px-4 hover:text-white text-sm font-medium text-center overflow-hidden hover:bg-primary focus:z-10 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-gray-400 rounded-lg">
+	<NuxtLink :class="active ? 'text-white bg-primary': 'text-primary bg-secondary'" :to="to" class="group relative min-w-0 flex-1 py-4 px-4 hover:text-white text-sm font-medium text-center overflow-hidden hover:bg-primary focus:z-10 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-gray-400 rounded-lg">
 		<slot name="icon"></slot>
 
 		<span class="block text-xs">{{ name }}</span>
