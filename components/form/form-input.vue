3b53a02bb80fbfb2621e1d84b1331f784463fa28
Mohamed jinas <j@live.mv>
2022-07-28 16:35:20 +0500

    WIP

--- components/form/form-input.vue
+++ components/form/form-input.vue
@@ -4,7 +4,7 @@
 			<span>{{ label }}</span>
 		</label>
 
-		<input :type="type" :placeholder="placeholder" @input="handleInput" class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-secondary focus:ring-secondary dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400" />
+		<input :type="type" :value="value" :placeholder="placeholder" @input="handleInput" class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-secondary focus:ring-secondary dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400" />
 		<div v-if="error">
 			<p class="mt-2 text-sm text-red-600">{{ error }}</p>
 		</div>
